a224c8a733af2bded3820e5398d30457bebc2cd5
lmegviar <lmegviar@gmail.com>
2022-01-20 18:48:07 -0500

    Set mysql service to enable query logging at runtime

--- files/mysql-runit.sh
+++ files/mysql-runit.sh
@@ -8,4 +8,4 @@ MYSQLADMIN='/usr/bin/mysqladmin'
 trap "$MYSQLADMIN shutdown" 0
 trap 'exit 2' 1 2 3 15
 
-exec  /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf & wait
+exec  /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --general_log=1 --general_log_file=/var/log/mysql/queries.log & wait
